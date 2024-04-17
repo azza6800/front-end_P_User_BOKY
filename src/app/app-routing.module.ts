91d00e139c26e3cec9281120d24a05f1232e0ec5
hazem lachheb <lachhebhazem87@gmail.com>
2024-04-17 12:22:46 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ContactComponent } from './contact/contact.component';
 import { RegisterComponent } from './register/register.component';
+import { AnnonceComponent } from './annonce/annonce.component';
 
 const routes: Routes = [  
 {path:'',component:HomeComponent},
 {path:'login',component:LoginComponent},
 {path:'Contact',component:ContactComponent},
 {path:'register',component:RegisterComponent},
+{path:'annonce',component:AnnonceComponent},
 
 ];
 
