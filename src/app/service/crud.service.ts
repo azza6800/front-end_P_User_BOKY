ae6755f52413fee9f9a2f5e13a8935f2cdafde33
hazem lachheb <lachhebhazem87@gmail.com>
2024-04-26 01:26:12 +0200

    hhg

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -3,9 +3,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Utilisateur } from '../Entites/Utilisateur.Entites';
 import { Contact } from '../Entites/Contact.Entites';
 import  {Observable} from 'rxjs';
-import { Annonce } from '../Entites/Annonce.Entites';
 import { JwtHelperService } from '@auth0/angular-jwt';
-
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +20,15 @@ export class CrudService {
   loginUtilisateur(utilisateur:Utilisateur){
     return this.http.post<any>(this.loginUserUrl, utilisateur);
   }
-
+  getUserInfo() {
+    var token = localStorage.getItem("myToken");
+    const helper = new JwtHelperService();
+    const decodedToken = helper.decodeToken(token);
+    const expirationDate = helper.getTokenExpirationDate(token);
+    const isExpired = helper.isTokenExpired(token);
+    var decoded: any
+    return decodedToken?.data
+  }
   //ContactCrud
   addContact(contact:Contact)
    {
@@ -35,19 +41,6 @@ export class CrudService {
   getContact(): Observable<Contact[]>{
     return this.http.get<Contact[]>(this.apiUrl + "/Contact");
   }
-
-  addAnnonce(annonce:Annonce)
-   {
-    return this.http.post<any>(this.apiUrl+"/annonce",annonce);
-   }
-   getAnnonce(): Observable<Annonce[]>{
-    return this.http.get<Annonce[]>(this.apiUrl + "/annonce");
-  }
-  onDeleteAnnonce(id : number){
-    const url =`${this.apiUrl+"/annonce"}/${id}` 
-    return this.http.delete(url)
-  }
-
   isLoggedIn(){
 
     let token = localStorage.getItem("myToken");
@@ -57,15 +50,6 @@ export class CrudService {
     } else {
       return false;
     }
-  }
-  getUserInfo() {
-    var token = localStorage.getItem("myToken");
-    const helper = new JwtHelperService();
-    const decodedToken = helper.decodeToken(token);
-    const expirationDate = helper.getTokenExpirationDate(token);
-    const isExpired = helper.isTokenExpired(token);
-    var decoded: any
-    return decodedToken?.data
   }
   
 }
