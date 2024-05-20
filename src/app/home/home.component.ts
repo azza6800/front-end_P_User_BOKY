b61fe0bb43bee24dc93bce0b43f79e33fc5dbe5f
hazem lachheb <lachhebhazem87@gmail.com>
2024-05-20 11:43:12 +0100

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Annonce } from '../Entites/Annonce.Entites';
+import { Router } from '@angular/router';
+import { CrudService } from '../service/crud.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  listAnnonce: Annonce[];
+  p:number=1;
+  collection:any[]
+  constructor(private service:CrudService,private router:Router ) { }
+ 
+ 
+  
+  detailannonce(id: number): void {
+    this.router.navigate(['/detailannonce', id]);
+  }
+ 
+ 
+  ngOnInit(): void {
+    this.service.getAnnonce().subscribe(annonce => {
+      this.listAnnonce = annonce
+    })
+  }
 
 }
